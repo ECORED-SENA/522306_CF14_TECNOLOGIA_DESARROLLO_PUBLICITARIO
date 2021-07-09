ddedb2ca0e8f1473bc6e489661e47aca41a39a9b
wroaserrano <walter.roas@misena.edu.co>
2021-07-09 09:54:22 -0500

    Update Tema2.vue

--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -286,8 +286,8 @@
       h2 2.3 Cálculo del ROI
     figure.mb-5
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
-      figcaption Video: Leyenda del video
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/PQR2ywfYTEI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+      //figcaption Video: Leyenda del video
 </template>
 <script>
 import LineaTiempoIcon from '../components/LineaTiempoIcon'
